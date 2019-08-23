afe3edafb7483486ae6a1cc4e50e0e66467cb7ee
edipkavvas <edipkavvas@hotmail.com>
2019-08-23 10:01:24 +0300

    fix

---
+++ src/components/alertBox/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+
+class AlertBox extends React.Component {}
+
+export default AlertBox;
